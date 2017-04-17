e5c40c60c322478c2faed0bc08cc0e8b2b736a06
Karan Oberoi & Sara \"Jerry\" Gerou <pair+karanobe.saragerou@devbootcamp.com>
2017-04-17 15:42:30 -0500

    Answers forms; update and edit

--- rails-skeleton/app/controllers/answers_controller.rb
+++ rails-skeleton/app/controllers/answers_controller.rb
@@ -6,6 +6,28 @@ class AnswersController < ApplicationController
     redirect_to @question
   end
 
+  def edit
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+
+    if @answer.update(answer_params)
+      redirect_to @question
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.find(params[:id])
+    @answer.destroy
+    redirect_to @question
+  end
 
   private
   def answer_params
